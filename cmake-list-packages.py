3c7048ba8d0e364061c48faeb183aecddad589d2
Frederik Braendstrup <fbraendstrup@roku.com>
2022-02-14 09:56:02 +0100

    made more the script more scriptable
    The title and underline messes with sort/uniq/grep/etc

--- cmake-list-packages.py
+++ cmake-list-packages.py
@@ -5,8 +5,6 @@ import glob
 
 
 def printPackages():
-    print('Packages')
-    print('========')
     paths = glob.glob(os.path.join('/usr/share', 'cmake*/Modules'))
     modules = [package for path in paths for package in os.listdir(path)]
     [print(module[4:-6]) for module in modules if re.search("^Find", module) and module[4:-6] != '']
